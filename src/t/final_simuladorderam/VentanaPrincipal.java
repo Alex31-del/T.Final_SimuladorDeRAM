3e446affdee96724187d5e752db1d0a889c3f20e
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-16 17:23:42 -0500

    commit_nuevasClases

---
+++ src/t/final_simuladorderam/VentanaPrincipal.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package t.final_simuladorderam;
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaPrincipal extends JFrame {
+    private MemoriaRAM  memoria;
+    private PanelProcesos pnlProcesos;
+    private PanelRam pnlRam;
+
+    public VentanaPrincipal() {
+        
+        memoria = new MemoriaRAM(1024);
+        pnlRam = new PanelRam(memoria);
+        pnlProcesos = new PanelProcesos(memoria, pnlRam);
+
+        
+        setTitle("Simulador de Memoria RAM");
+
+        setLayout(new BorderLayout());
+
+        add(pnlProcesos, BorderLayout.WEST);
+        add(pnlRam, BorderLayout.CENTER);
+
+        setSize(800,600);
+
+        setLocationRelativeTo(null);
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setVisible(true);
+}
