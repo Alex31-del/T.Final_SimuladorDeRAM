b2cd9d1c0eb6122200c1b47c425163253bccaeb0
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-17 13:39:57 -0500

    commit

--- src/t/final_simuladorderam/ClasePrincipal.java
+++ src/t/final_simuladorderam/ClasePrincipal.java
@@ -12,13 +12,13 @@ public class ClasePrincipal{
     
     public static void main(String[] args) {
         try {
-        for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
-            if ("Nimbus".equals(info.getName())) {
-                UIManager.setLookAndFeel(info.getClassName());
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                     UIManager.setLookAndFeel(info.getClassName());
                 break;
+               }
             }
-        }
-    } catch (Exception e) {
+       } catch (Exception e) {
        
     }
     
