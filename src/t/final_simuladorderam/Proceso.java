0b0e027967499eba4e5aaa2b4edaf2e638755412
Alex31-del <alexandernavarroseminario@gmail.com>
2026-07-23 01:32:31 -0500

    commit

--- src/t/final_simuladorderam/Proceso.java
+++ src/t/final_simuladorderam/Proceso.java
@@ -3,31 +3,10 @@ package t.final_simuladorderam;
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-import java.awt.Color;
-/**
+ 
  *
  * @author user
  */
 public class Proceso {
-    private String nombre;
-    private  int memoria;
-    private Color color;
-
-    public Proceso(String nombre, int memoria, Color color) {
-        this.nombre = nombre;
-        this.memoria = memoria;
-        this.color=color;
-        
-    }
-    
-    public String getNombre(){
-        return nombre;
-    }
-    public int getMemoria(){
-        return memoria;
-    }
-    public Color getColor(){
-        return color;
-    }
+   
 }
